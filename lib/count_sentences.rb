24e8c72bdc356f7d55b6b3ce3130b82a7ade3f4d
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-14 01:25:30 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+     self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    arr_of_str = self.split
+    arr_of_str.count do |word|
+       word.sentence? | word.question? | word.exclamation?
+    end   
   end
 end
